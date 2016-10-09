d57909f2f84081a610cc2a3e383f78dc48bd6529
Gokulakannan451 <gokulakannan.451@gmail.com>
2016-10-09 21:57:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+
+//change the text of the main - text div
+var element = document.getElementById("main-text'):
+
+element.innerHTML = 'new value';
+var img = document.getElementById('madi');
+var marginleft = 0;
+function moveright () {
+        marginleft = marginleft+10;
+        img.style.moveleft
+}
